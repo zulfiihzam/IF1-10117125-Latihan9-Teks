79be3bba9a59d6df7256e558131bfa5e466955ed
zulfiihzam <35595962+zulfiihzam@users.noreply.github.com>
2020-10-21 00:46:54 +0700

    Add files via upload

---
+++ IF1-10117125-Latihan9-Teks/src/if1/pkg10117125/latihan9/teks/IF110117125Latihan9Teks.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10117125.latihan9.teks;
+
+/**
+ *
+ * @author 
+ * Nama  : Zulfi Ihzam Rahmat
+ * Kelas : IF 1
+ * Nim   : 10117125
+ * Deskripsi Program : berisi program yang menampilkan
+ * hasil character
+ */
+public class IF110117125Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+    }
+    
+}
